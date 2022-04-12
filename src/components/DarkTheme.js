0b0f4f4610026abb528ae4b57413dbff9734992a
VitorHRD <vrdev435@gmail.com>
2022-04-11 23:17:37 -0300

    origin

--- src/components/DarkTheme.js
+++ src/components/DarkTheme.js
@@ -1,33 +1,33 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext } from 'react'
 import { GameContext } from '../context/context'
 
 
 export default function DarkTheme(props) {
-    let {emoji} = useContext(GameContext)
-    let [emojiValue , setEmojiValue] = emoji;
-    if(emojiValue===true){
-     document.body.style.backgroundColor= "#101c2c";
-     localStorage.setItem("theme","dark");
+    let { emoji } = useContext(GameContext);
+    let [emojiValue, setEmojiValue] = emoji;
+    if (emojiValue === true) {
+        document.body.style.backgroundColor = "#101c2c";
+        localStorage.setItem("theme", "dark");
     }
-    else{
-        document.body.style.backgroundColor= "white";
-        localStorage.setItem("theme","light");
+    else {
+        document.body.style.backgroundColor = "white";
+        localStorage.setItem("theme", "light");
     }
-    return (emojiValue=== false? 
+    return (emojiValue === false ?
         <div className="toggle">
-        <div id="emoji" >🌞</div>
-        <input type="checkbox" id="foo"></input>
-        <label htmlFor="foo" onClick={()=>{
-            setEmojiValue(emojiValue === true ? false : true)
-        }}></label>
-    </div>
-    :
-    <div className="toggle">
-    <div id="emoji2">🌜</div>
-    <input type="checkbox" id="foo"></input>
-    <label htmlFor="foo" onClick={()=>{
-            setEmojiValue(emojiValue === true ? false : true)
-        }}></label>
-</div>
+            <div id="emoji" >🌞</div>
+            <input type="checkbox" id="foo"></input>
+            <label htmlFor="foo" onClick={() => {
+                setEmojiValue(emojiValue === true ? false : true)
+            }}></label>
+        </div>
+        :
+        <div className="toggle">
+            <div id="emoji2">🌜</div>
+            <input type="checkbox" id="foo"></input>
+            <label htmlFor="foo" onClick={() => {
+                setEmojiValue(emojiValue === true ? false : true)
+            }}></label>
+        </div>
     )
 }
